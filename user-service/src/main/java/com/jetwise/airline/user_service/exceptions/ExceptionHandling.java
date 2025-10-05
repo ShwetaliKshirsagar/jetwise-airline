ce0a8230744ec5e12cb2560b53070ad0733f445d
Shwetali <shwetalikshirsagar42944@gmail.com>
2025-10-05 15:13:20 +0100

    corrected class name

--- user-service/src/main/java/com/jetwise/airline/user_service/exceptions/ExceptionHandling.java
+++ user-service/src/main/java/com/jetwise/airline/user_service/exceptions/ExceptionHandling.java
@@ -5,20 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class ExeceptionHandler {
+public class ExceptionHandling {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Map<String, Object>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
         HashMap<String, Object> error = new HashMap<>();
         error.put("status", HttpStatus.BAD_REQUEST.value());
         error.put("timestamp", LocalDateTime.now());
-        error.put("error", "User Already Exists");
+        error.put("error", ex.getCause());
         error.put("message", ex.getMessage());
 
         return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
