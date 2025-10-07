26396ede8b53b1d899e82c26670105c99719d1a2
Shwetali <shwetalikshirsagar42944@gmail.com>
2025-10-07 21:41:03 +0100

    Created separate module for JWT generation and validation.

---
+++ jwt-common/src/test/java/com/jetwise_airline/jwt_common/JwtCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetwise_airline.jwt_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
